a0a0cfc0dc3c83e5bf9c1134f36ffada740e1c5f
Deniz Ariyan <denizariyan@gmail.com>
2020-10-12 04:19:07 +0300

    Display image correctly

--- gtavc-map/gtavc-map.cpp
+++ gtavc-map/gtavc-map.cpp
@@ -1,20 +1,22 @@
-// gtavc-map.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include <Eigen/Core>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/eigen.hpp>
+using namespace cv;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat I = imread("C:\\Users\\Deniz\\Pictures\\IMG_14540.jpg", IMREAD_UNCHANGED);
+	if (I.empty())
+	{
+		std::cout << "!!! Failed imread(): image not found" << std::endl;
+		// don't let the execution continue, else imshow() will crash.
+	}
+
+	namedWindow("Display window", CV_WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Display window", I);
+	waitKey(0);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
