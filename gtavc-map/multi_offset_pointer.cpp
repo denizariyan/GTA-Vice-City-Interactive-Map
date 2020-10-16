540dd0d96351bf43b0e537572cd8e06bf2cc26fd
Deniz Ariyan <30367397+denizariyan@users.noreply.github.com>
2020-10-16 18:06:39 +0300

    Change comments for visibility
    Change credit comment location for better visibility

--- gtavc-map/multi_offset_pointer.cpp
+++ gtavc-map/multi_offset_pointer.cpp
@@ -1,9 +1,11 @@
+// Credit: https://stackoverflow.com/a/56310967
+
 #include <windows.h>
 #include <conio.h>
 #include <iostream>
 #include <vector>
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) // Credit for this function only: https://stackoverflow.com/a/56310967
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
 {
 	for (unsigned int i = 0; i < offsets.size(); ++i)
 	{
